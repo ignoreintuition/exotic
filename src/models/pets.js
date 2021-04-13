33bf77c9d95c6466aeb5f8d81d9b8bbcb1a019e9
Brian Greig <brian@resurgencewebdesign.com>
2021-04-13 01:15:06 -0400

    query pipeline

--- src/models/pets.js
+++ src/models/pets.js
@@ -10,12 +10,45 @@ const client = new MongoClient(uri, {
 const petsDb = async query => {
     try {
         await client.connect();
-        const database = await client.db('exotic');
-        const userCollection = await database.collection('users')
-        const user = await userCollection.findOne(query);
-        const petsCollection = await database.collection('pets');
-        const pets = await petsCollection.find({owner: user._id});
-        return pets.toArray();
+        const pipeline = [
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner_pet"
+                }
+            },
+            {
+                $unwind:"$owner_pet"
+            },
+            {
+                $lookup: {
+                    from: "animals",
+                    localField: "species",
+                    foreignField: "_id",
+                    as: "pet_species"
+                }
+            },
+            {
+                $unwind:"$pet_species"
+            },
+            {
+                $match:{
+                    $and:[{"owner_pet.firstName" : query.firstName }]
+                }
+            },
+            {
+                $project: {
+                    name: 1,
+                    ownerName: "$owner_pet.firstName",
+                    species: "$pet_species.species"
+                }
+            }];
+        const aggCursor =  await client.db('exotic')
+            .collection('pets')
+            .aggregate(pipeline);
+        return aggCursor.toArray();
     } catch(e) {
         throw new Error(e.message);
     }
